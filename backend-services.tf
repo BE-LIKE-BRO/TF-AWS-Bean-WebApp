87528ba156e5c4e3ebc06429cf8d13ae50a29540
Ayoola Coulibaly <ayoolacoul27@gmail.com>
2022-04-21 12:21:01 +0100

    added backend services

---
+++ backend-services.tf
@@ -0,0 +1,58 @@
+resource "aws_db_subnet_group" "rds-subnet-grp" {
+  name = "main"
+  subnet_ids = [module.vpc.private_subnets[0], module.vpc.private_subnets[1], module.vpc.private_subnets[2]]
+  tags = {
+    Name = "Subnet group for RDS"
+  }
+}
+
+resource "aws_elasticache_subnet_group" "elasticache-subnet-grp" {
+  name = "elasticache-subnet-grp"
+  subnet_ids = [module.vpc.private_subnets[0], module.vpc.private_subnets[1], module.vpc.private_subnets[2]]
+  tags = {
+      Name = "subnet froup for elasticache"
+  }
+}
+
+resource "aws_db_instance" "BLB-RDS" {
+    allocated_storage = 20
+    storage_type = "gps"
+    engine = "mysql"
+    engine_version = "5.6.34"
+    instance_class = "db.t2.micro"
+    name = var.dbname
+    username = var.dbuser
+    password = var.dbpass
+    parameter_group_name = "default.mysql5.6"
+    multi_az = "false"
+    publicly_accessible = "false"
+    skip_final_snapshot = true
+    db_subnet_group_name = aws_db_subnet_group.rds-subnet-grp.name
+    vpc_security_group_ids = [aws_security_group.Backend-SG.id]
+}
+
+resource "aws_elasticache_cluster" "BLB-cache" {
+  cluster_id = "BLB-cache"
+  engine = "memcached"
+  node_type = "cache.t2.micro"
+  num_cache_nodes = 1
+  parameter_group_name = "default.memcached1.5"
+  port = 11211
+  security_group_ids = [aws_security_group.Backend-SG.id]
+  subnet_group_name = aws_elasticache_subnet_group.elasticache-subnet-grp.name
+}
+
+resource "aws_mq_broker" "name" {
+  broker_name = "BLB-rmq"
+  engine_type = "ActiveMQ"
+  engine_version = "5.15.0"
+  host_instance_type = "mq.t2.micro"
+  security_groups = [aws_security_group.Backend-SG.id]
+  subnet_ids = [module.vpc.private_subnets[0]]
+
+  user {
+    username = var.rmquser
+    password = var.rmqpass
+  }
+
+}       
